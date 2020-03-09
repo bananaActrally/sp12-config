a38fbefc34ce73251a32ee1bc1b031d9efcc23b2
zuul <369969653@qq.com>
2020-03-09 11:18:01 +0800

    testGit2

--- src/test/java/cn/tedu/sp12/Sp12ConfigApplicationTests.java
+++ src/test/java/cn/tedu/sp12/Sp12ConfigApplicationTests.java
@@ -1,13 +1,32 @@
 package cn.tedu.sp12;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
+import java.util.HashMap;
+import java.util.Map;
+
+//@SpringBootTest
 class Sp12ConfigApplicationTests {
+	public static Map map = new HashMap();
+	{
+		map = new HashMap();
+		map.put("1","2");
+	}
+	public Map getMap() {
+		return map;
+	}
+
+	public void setMap(Map map) {
+		this.map = map;
+	}
 
 	@Test
 	void contextLoads() {
+		Map tMap = map;
+		tMap.put("1","t");
+		System.out.println(tMap.get("1"));
+		System.out.println(map.get("1"));
+//		map.put();
 	}
 
 }
